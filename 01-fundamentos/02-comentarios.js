73042eaefff88599c496d85e2bc566191485e15d
Allan Gadelha <allangadelha@gmail.com>
2021-04-17 21:30:24 -0300

    fundamentos: comentários

---
+++ 01-fundamentos/02-comentarios.js
@@ -0,0 +1,15 @@
+// comentários de um linha
+console.log('Linha 01')
+
+/*
+    comentários de 
+    multiplas linhas
+*/
+console.log('Linha 02')
+
+/*
+ * comentários de 
+ * multiplas linhas
+ */
+
+console.log('Linha 03')
