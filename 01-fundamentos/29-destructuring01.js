53e3615b2dd8c28c5e3e6845b3f72329e48376f2
Allan Gadelha <allangadelha@gmail.com>
2021-04-26 23:58:30 -0300

    fundamentos: destructuring 01

---
+++ 01-fundamentos/29-destructuring01.js
@@ -0,0 +1,22 @@
+// novo 
+
+const pessoa = {
+    nome: 'Ana',
+    idade: 5,
+    endereco: {
+        logradouro: 'Rua Boa',
+        numero: 123
+    }
+}
+
+const { nome, idade } = pessoa
+console.log(nome, idade)
+
+const { nome: n, idade: i } = pessoa
+console.log(n, i)
+
+const { sobrenome, bemHumorada = true } = pessoa
+console.log(sobrenome, bemHumorada)
+
+const { endereco: { logradouro, numero, cep} } = pessoa
+console.log(logradouro, numero, cep)
